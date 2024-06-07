e5b6a0550c6a501249ac3dfecad9203c00bdf1b7
Rafael Honorio Venetikides <rafael.venetikides@pucpr.edu.br>
2024-06-07 12:32:58 -0300

    mainpage script creation

---
+++ mainpage.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function(){
+    var topLogo = document.getElementById('topLogo');
+
+    topLogo.addEventListener('click', function(){
+        window.location.href = 'mainpage.html';
+    });
+});
